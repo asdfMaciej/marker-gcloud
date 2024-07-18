ddde189712335474b7c59e2c3ddd7e9dd61295ca
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-07-18 14:42:30 +0200

    fix: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+# Run as elevated user
 
+apt-get install -y python3.11-venv
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 python3 -m venv ~/local
-
-sudo su 
 python3 install_gpu_driver.py
 source ~/local/bin/activate
-apt-get install python3-pip python3-opencv
+apt-get install -y python3-pip python3-opencv
 pip3 install torch torchvision torchaudio
 pip3 install fastapi uvicorn marker-pdf
 
