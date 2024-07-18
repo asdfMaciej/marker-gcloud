85cddd7d3db81493d7dfedfdcdbdaf8440a267f9
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-07-18 14:30:33 +0200

    feat: add GCloud setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+python3 -m venv ~/local
+
+sudo su 
+python3 install_gpu_driver.py
+source ~/local/bin/activate
+apt-get install python3-pip python3-opencv
+pip3 install torch torchvision torchaudio
+pip3 install fastapi uvicorn marker-pdf
+
+# Now it's possible to run the application:
+# uvicorn app.main:app --host 0.0.0.0 --port 80
